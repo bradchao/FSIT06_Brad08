263779e5cc3380f6058f0576f5dc3ce01f842bf0
Brad <brad@brad.tw>
2019-07-28 14:12:50 +0800

    03

--- app/src/main/java/tw/org/iii/appps/brad08/MyView.java
+++ app/src/main/java/tw/org/iii/appps/brad08/MyView.java
@@ -13,13 +13,19 @@ import java.util.HashMap;
 import java.util.LinkedList;
 
 public class MyView extends View {
-    private LinkedList<HashMap<String,Float>> line;
+    private LinkedList<LinkedList<HashMap<String,Float>>> lines;
+    private Paint paint;
 
     public MyView(Context context, AttributeSet attrs) {
         super(context, attrs);
         setBackgroundColor(Color.GREEN);
 
-        line = new LinkedList<>();
+        lines = new LinkedList<>();
+
+        paint = new Paint();
+        paint.setStrokeWidth(10);
+        paint.setColor(Color.BLUE);
+
 
 //        setOnClickListener(new OnClickListener() {
 //            @Override
@@ -34,25 +40,29 @@ public class MyView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        Paint paint = new Paint();
-        paint.setStrokeWidth(10);
-        paint.setColor(Color.BLUE);
-
-        for (int i=1; i<line.size(); i++){
-            HashMap<String,Float> p0 = line.get(i-1);
-            HashMap<String,Float> p1 = line.get(i);
-            canvas.drawLine(p0.get("x"),p0.get("y"),
-                    p1.get("x"),p1.get("y"), paint);
+        for (LinkedList<HashMap<String,Float>> line : lines) {
+            for (int i = 1; i < line.size(); i++) {
+                HashMap<String, Float> p0 = line.get(i - 1);
+                HashMap<String, Float> p1 = line.get(i);
+                canvas.drawLine(p0.get("x"), p0.get("y"),
+                        p1.get("x"), p1.get("y"), paint);
+            }
         }
-
-
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
+
         HashMap<String,Float> point = new HashMap<>();
         point.put("x", event.getX()); point.put("y", event.getY());
-        line.add(point);
+
+        if (event.getAction() == MotionEvent.ACTION_DOWN){
+            LinkedList<HashMap<String,Float>> line = new LinkedList<>();
+            line.add(point);
+            lines.add(line);
+        }else if (event.getAction() == MotionEvent.ACTION_MOVE){
+            lines.getLast().add(point);
+        }
 
         invalidate();   // repaint
 
